368e75e1d2eac8af3c50083f3b07f0657804ea4c
chanitnan0jr <ch4n1tnan@gmail.com>
2025-10-30 23:58:59 +0700

    docs: add ST217 weekly project footer with author info

--- week1_programming/main.c
+++ week1_programming/main.c
@@ -1,3 +1,8 @@
+// ================================================================
+//  End Credit:
+//  Developed by Chanitnan Kitnantakhun (Chanitnan0jr on GitHub)
+//  Weekly C Project for ST217 (Statistics)
+// ================================================================
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
